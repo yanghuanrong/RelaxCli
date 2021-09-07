bda46a5e4dce1843d2800848633d521753d7b8f6
yang huan rong 杨焕荣 <yanghr10@cn.midea.com>
2021-09-07 16:03:56 +0800

    feat: 实现vue编译

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,10 +1,8 @@
-// import Vue from 'vue';
-// import App from './App.vue';
+import Vue from 'vue';
+import App from './App.vue';
 
-// Vue.config.productionTip = false;
+Vue.config.productionTip = false;
 
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount('#app');
-
-console.log('1');
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
